08424903bae99d3f0a0078da0ec45ab8e0e97465
xxjcaxx <jocasal@gmail.com>
2024-10-04 08:34:23 +0200

    school

---
+++ school/models/models.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class student(models.Model):
+     _name = 'school.student'
+     _description = 'Students of school'
+
+     name = fields.Char()
+     year = fields.Integer()
+     surname = fields.Char(
+          string="Surnames",  # Optional label of the field
+          required = True,  # Mandatory field
+          help = 'blabla',  # Help tooltip text
+     )
+     photo = fields.Image(max_width = 200, max_height = 200)
+     type = fields.Selection([
+          ('1','Basic'),
+          ('2','Intermediate'),
+          ('3','Completed')])
+
+
+class teacher(models.Model):
+     _name = 'school.teacher'
+     _description = 'Teachers of school'
+
+     name = fields.Char()
+     dni = fields.Char()

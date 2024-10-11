ce47a9534531f81be4431a2e251d52c8c5ef2ebc
davpalmol <davpalmol@alu.edu.gva.es>
2024-10-11 07:24:56 +0000

    Problems solved

--- game/models/forja.py
+++ game/models/forja.py
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
 
-
-class forja(models.Model):
-    _name = 'game.forja'
-    _description = 'game.forja'
-
-    name = fields.Char()
-    description = fields.Text()
-
-
-=======
-
-from odoo import models, fields, api  # Aquí se importa 'models'
 
 class forja(models.Model):
     _name = 'game.game'
@@ -22,4 +9,5 @@ class forja(models.Model):
 
     name = fields.Char()
     description = fields.Text()
->>>>>>> 5e743e72fe14120bc84c3de0bb98ca2c943e323a
+
+

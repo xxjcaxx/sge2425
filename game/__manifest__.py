8f48d086e63e719e230621b2b4225660621282f6
PapaFrita23 <adrianvernicholtra@gmail.com>
2024-10-18 07:46:28 +0000

    Fer tipus de characters

--- game/__manifest__.py
+++ game/__manifest__.py
@@ -22,8 +22,9 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
+        'views/typeCharacter.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode

0d47a7007d8f9144a597093bb4af4295ac7cec70
miroslava <miv986@hotmail.com>
2024-10-25 09:16:59 +0200

    Relaciones Character, typeCharacter, Object y objectType

--- game/models/characters.py
+++ game/models/characters.py
@@ -8,11 +8,12 @@ class characters(models.Model):
     _description = 'game.characters'
 
     player = fields.Many2one('game.model_player', string='Players')
+    characterType = fields.Many2one('game.type_character',
+                                    string='Character Type')
     name = fields.Char()
     level = fields.Integer()
     exp = fields.Integer()
     stats = fields.Char()
-    objects = fields.Char()
+    objects = fields.One2many('game.object', 'character',
+                              string='Objects')
     picture = fields.Image(max_width=200, max_height=200)
-
-

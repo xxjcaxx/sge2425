cdd58a78b79d725da4ca623434b4a92839927dce
manelNunez <manel.nunezfer@gmail.com>
2024-10-25 09:14:14 +0200

    Views arreglado, creado: dragon_type

--- game/models/dragon.py
+++ game/models/dragon.py
@@ -11,12 +11,4 @@ class dragon(models.Model):
     timeout = fields.Integer()
     expGained = fields.Integer()
     avatar = fields.Image(max_width = 200, max_height = 200)
-    type = fields.Selection([
-        ('1', 'Mushu'),
-        ('2', 'Toothless'),
-        ('3', 'EnderDragon'),
-        ('4', 'Charizard'),
-        ('5', 'Shenlong'),
-        ('6', 'Polunga'),
-        ('7', 'Smaug'),
-        ('8', 'Ryūjin')])
+    type = fields.Char(related='dragon_type.type')
---
+++ game/models/dragon_type.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class dragon(models.Model):
+    _name = 'game.dragon'
+    _description = 'game.dragon'
+
+    name = fields.Char()
+    avatar = fields.Image(max_width = 200, max_height = 200)
+    type = fields.Selection([
+        ('1', 'Mushu'),
+        ('2', 'Toothless'),
+        ('3', 'EnderDragon'),
+        ('4', 'Charizard'),
+        ('5', 'Shenlong'),
+        ('6', 'Polunga'),
+        ('7', 'Smaug'),
+        ('8', 'Ryūjin')],
+        required = True)

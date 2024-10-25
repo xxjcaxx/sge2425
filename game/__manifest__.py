50800225e972571c30cd6538756817fee3cd623d
pautpdam <pautpdam@gmail.com>
2024-10-25 09:46:48 +0200

    Insertar demomaps.xml en manifest

--- game/__manifest__.py
+++ game/__manifest__.py
@@ -35,7 +35,8 @@ Long description of module's purpose
         'views/forja.xml',
         'views/templates.xml',
         'views/object_type.xml',
-        'views/model_player.xml'
+        'views/model_player.xml',
+        'demo/demomaps.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
